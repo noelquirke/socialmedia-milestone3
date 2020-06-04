2056f04f90fead0f87913e575e02e68f19385f79
Noel Quirke <noelquirke900@hotmail.com>
2020-06-04 12:16:51 +0000

    Fixing 'NameError: name 'DataRequired' is not defined'

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
-from wtforms.validators import *
+from wtforms import *
 from models import User
 
 def name_exist( form, field ):
